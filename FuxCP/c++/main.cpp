3c54cc565dbf5a44c92664931d263885f95aadd7
Arno Geurts <48802168+arnocomment@users.noreply.github.com>
2023-08-14 11:48:18 +0200

    Initial commit : running version of a simple gecode problem

---
+++ FuxCP/c++/main.cpp
@@ -0,0 +1,58 @@
+#include "headers/gecode_problem.hpp"
+
+using namespace Gecode;
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    std::cout << "starting main" << std::endl;
+    // 4 lists, one per beat and each element is the measure
+    const int nRows = 4;      // TODO next : take these values from argv
+    const int nCols = 11;     // same
+    int cantus_firmus_values[nRows][nCols] = {
+        {62,65,64,62,67,65,69,67,65,64,62}, // no elements in other beats since this is first species
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    };
+    // allocate memory for 2D array (array of pointers to int arrays)
+    int** cantus_firmus = new int*[nRows];
+    for (int i = 0; i < nRows; ++i) {
+        cantus_firmus[i] = new int[nCols];
+    }
+
+    // Initialize the array 
+    for (int i = 0; i < nRows; ++i) {
+        for (int j = 0; j < nCols; ++j) {
+            cantus_firmus[i][j] = cantus_firmus_values[i][j];
+        }
+    }
+
+    
+    // using 1D array for better cache performance? -> access elements with array[x*cols + y] ?
+    // create a new problem
+    Problem* p = new Problem(cantus_firmus);
+
+    // create a new search engine
+    Search::Base<Problem>* e = make_solver(p, bab_solver);
+    delete p;
+
+    int nb_sol = 0;
+
+    while(Problem * pb = get_next_solution_space(e)){
+        nb_sol++;
+        cout << "Solution " << nb_sol << ": " << endl;
+        pb->print_solution();
+        delete pb;
+    }
+    cout << "No (more) solutions." << endl;
+
+    // Clean up memory
+    for (int i = 0; i < nRows; ++i) {
+        delete[] cantus_firmus[i];
+    }
+    delete[] cantus_firmus;
+
+    return 0;
+    
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This is a temporary structure to create CP problems with gecode with OpenMusic in Lisp, via the GiL framework
